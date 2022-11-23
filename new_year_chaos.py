b063a57c1ba85304536de3d0879f91f65a69a999
emmacodes <binma.emma@gmail.com>
2022-11-23 14:59:39 -0800

    new year chatic

---
+++ new_year_chaos.py
@@ -0,0 +1,31 @@
+# One person can bribe at most two others.
+#Determine the minimum number of bribes that took place to get to a given queue order. 
+# Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
+#solution1 missed some corner case, such as [1 2 5 3 7 8 6 4], '4' is not taken into account
+def minimumBribes(q):
+    
+    bribe = 0
+    jump = 0 
+    for i in range(len(q)):
+        if q[i] > i:
+            if q[i]-1-i >2:
+                print('Too chaotic')
+                return
+            else:
+                jump = q[i]-1 -i
+                bribe+= jump
+    print(bribe)
+    return
+
+#solution 2 
+def minimumBribes(q):
+    bribes = 0
+    for i in range(len(q)-1, -1, -1):
+        if q[i] - (i + 1) > 2:
+            print('Too chaotic')
+            return
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribes += 1
+    print(bribes)
