1736494f03bdbdd1b4ee8c04eb67a8f5edfd8a9b
emmacodes <binma.emma@gmail.com>
2022-12-05 12:17:18 -0800

    two sum using dictionary

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/two-sum/description/
+#to optimize this question. I can use dictionary to increase time complexity to O(n)
+#space complexity to O(n)
+
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    lookup_dict={}
+    for index, num in enumerate(nums):
+        difference= target - num
+        if difference in lookup_dict:
+            return[index, lookup_dict[difference]]
+        lookup_dict[num] = index
+
+#dictionary is very useful for quick lookup, instead of looping twice, using dictionary
+#will only loop once
