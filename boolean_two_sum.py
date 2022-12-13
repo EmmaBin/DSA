647bcc36277f2cc910b78585fd2629fbabb0f729
emmacodes <binma.emma@gmail.com>
2022-12-13 15:27:45 -0800

    O(n) time complexity using two pointers

--- boolean_two_sum.py
+++ boolean_two_sum.py
@@ -15,3 +15,20 @@ def check_target(lst, num):
   return False
 
 print(check_target([1, 2, 4, 6, 8, 9, 14, 15], 13))
+
+
+
+def check_for_target(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        curr = nums[left] + nums[right]
+        if curr == target:
+            return True
+        if curr > target:
+            right -= 1
+        else:
+            left += 1
+    
+    return False
