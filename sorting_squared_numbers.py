baf1fb0138833d9c976902b0c92a9d1372523502
emmacodes <binma.emma@gmail.com>
2022-12-15 14:00:43 -0800

    two pointers

--- sorting_squared_numbers.py
+++ sorting_squared_numbers.py
@@ -5,3 +5,18 @@
 # Input: nums = [-4,-1,0,3,10]
 # Output: [0,1,9,16,100]
 
+def sortedSquares(self, nums: List[int]) -> List[int]:
+    left=0
+    right=len(nums)-1
+    result=[0]* len(nums)
+    for i in range(len(nums)-1, -1, -1):
+        if abs(nums[left]) > abs(nums[right]):
+            to_square_num = nums[left]
+            left+=1
+            
+        else:
+            to_square_num=nums[right]
+            right-=1
+        result[i] = to_square_num*to_square_num
+    return result
+                
---
+++ valid_palindrome2.py
@@ -0,0 +1,3 @@
+#https://leetcode.com/problems/valid-palindrome-ii/
+#Given a string s, return true 
+# if the s can be palindrome after deleting at most one character from it.
