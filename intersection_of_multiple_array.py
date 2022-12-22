844b907bcfbd5424a09947db2d8c6f2861284a1b
emmacodes <binma.emma@gmail.com>
2022-12-21 23:38:21 -0800

    hashmap

---
+++ intersection_of_multiple_array.py
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/intersection-of-multiple-arrays/description/
+
+
+from collections import defaultdict
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        res=[]
+        #distinct meaning only appears once in each integer array,
+        #create a hash map of this nums array, if any number appears
+        #the amount of the total integer array, that is the number returned 
+
+        counts = defaultdict(int)
+        for i in nums:
+            for x in i:
+                counts[x] +=1
+        
+        nums_length= len(nums)
+        for key in counts:
+            if counts[key] == nums_length:
+                res.append(key)
+
+        return sorted(res)
