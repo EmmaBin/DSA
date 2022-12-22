96eba46bcfabef9ef71305c36adaccfc672adc31
emmacodes <binma.emma@gmail.com>
2022-12-21 23:51:10 -0800

    hashmap and set

--- check_good_or_bad_string.py
+++ check_good_or_bad_string.py
@@ -13,3 +13,15 @@ class Solution:
                 return False
 
         return True
+
+
+from collections import defaultdict
+
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        counts = defaultdict(int)
+        for c in s:
+            counts[c] += 1
+        
+        frequencies = counts.values()
+        return len(set(frequencies)) == 1
