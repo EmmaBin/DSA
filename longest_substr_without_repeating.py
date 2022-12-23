5c499c4da612d6bb66636bc2efee852c541c906d
emmacodes <binma.emma@gmail.com>
2022-12-23 13:57:40 -0800

    sliding window

--- longest_substr_without_repeating.py
+++ longest_substr_without_repeating.py
@@ -18,3 +18,25 @@ class Solution:
            
             max_len = max(max_len, len(new_lst))
         return max_len
+
+from collections import Counter
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = Counter()
+
+        left = right = 0
+
+        res = 0
+        while right < len(s):
+            r = s[right]
+            chars[r] += 1
+
+            while chars[r] > 1:
+                l = s[left]
+                chars[l] -= 1
+                left += 1
+
+            res = max(res, right - left + 1)
+
+            right += 1
+        return res
