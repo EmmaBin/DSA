2166e73370ddf21da615711663f51ab2aba6fa10
emmacodes <binma.emma@gmail.com>
2022-12-23 14:14:07 -0800

    optimized sliding window

--- longest_substr_without_repeating.py
+++ longest_substr_without_repeating.py
@@ -40,3 +40,21 @@ class Solution:
 
             right += 1
         return res
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        # mp stores the current index of a character
+        mp = {}
+
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+
+            ans = max(ans, j - i + 1)
+            mp[s[j]] = j + 1
+
+        return ans
