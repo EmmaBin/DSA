3bc2150710947475ee868f99f23923633d57b786
emmacodes <binma.emma@gmail.com>
2022-12-28 22:56:40 -0800

    different way

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -21,3 +21,17 @@ class Solution:
 
 
 #O(n) and O(n), bcz using hash takes up O(n) space
+
+def isValid(s):
+    stack =[]
+    match={'(':')', '{':'}', '[':']'}
+
+    for char in s:
+        if char in match:
+            stack.append(char)
+        else:
+            #s=']' 只有右邊，沒有辦法append，所以stack是空的 還應該return false 
+            if not stack or char!= match[stack.pop()]:
+                return False
+    
+    return len(stack)==0
