368bda6a11400893fca829b57ddb3984a1de0e57
emmacodes <binma.emma@gmail.com>
2023-01-17 17:18:19 -0800

    fixed sliding window

---
+++ min_vowels_0f_given_length.py
@@ -0,0 +1,32 @@
+#https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+   
+        #clarification: all lower case, s not empty, k >=1, if k>len(s), return -1
+        #return 1 is true, return 0 is false, return any negative number meaningfunction couldn't find the output wanted (indexing).
+
+        #using fixed sized siliding window and prefix method
+        #get the first k str to count how many vowels there
+        #get hold of the vowels count
+        # loop over the rest of the str, 
+        # keep adding one element in, delete the first out maintain the size of k
+        # while addding and deleting, count the vowels 
+        # keep updating the max and return 
+
+        vowels = 'aeiou'
+        left = 0
+        count=0
+        for i in range(0, k):
+            if s[i] in vowels:
+                count+=1
+        #assign to max_count first, later we need to keep track of this max
+        max_count = count
+        for i in range(k, len(s)):
+            if s[i] in vowels:
+                count +=1
+            if s[left] in vowels:
+                count -=1
+            left +=1
+            max_count = max(count, max_count)
+        return max_count
