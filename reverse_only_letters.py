5775166173efdc6b23a0e49e7ec43504a9067bca
emmacodes <binma.emma@gmail.com>
2023-01-18 12:10:23 -0800

    second attempt, intuitive way

--- reverse_only_letters.py
+++ reverse_only_letters.py
@@ -11,6 +11,8 @@ class Solution:
         while j>i:
             if not s_list[i].isalpha():
                 i+=1
+                #if there is no continue, it won't work, bcz
+                #the function of continue here means 
                 continue
             if not s_list[j].isalpha():
                 j-=1
@@ -20,3 +22,19 @@ class Solution:
             j-=1
 
         return ''.join(s_list)
+
+#intuitive way
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        result =[]
+        #create a list only container the alpha in original order
+        only_letter=[char for char in s if char.isalpha()]
+
+        for word in s:
+            if word.isalpha():
+                #if it's alpha, means this place needs to be replaced with the last word from only_letter
+                result.append(only_letter.pop())
+            else:
+                #if not, I can just place it in the original index number 
+                result.append(word)
+        return ''.join(result)
