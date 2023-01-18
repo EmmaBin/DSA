0c8054f40d88df1434ef131c65553b5d0c7a4356
emmacodes <binma.emma@gmail.com>
2023-01-18 14:35:27 -0800

    second attempt

--- min_subarray_lessequal_sum.py
+++ min_subarray_lessequal_sum.py
@@ -11,6 +11,10 @@ class Solution:
         for right in range(len(nums)):
             curr_sum += nums[right]
             while curr_sum >= target:
+                #result should be checked first
+                #这时候，left还在满足条件的位置，每一个满足条件的位置都应该被记下来
+                #所以 update result 要在while loop 里面，而且是第一个应该被check
+                #如果左边，移动了一位，还满足条件，再被check
                 result = min(result, right-left+1)
                
                 curr_sum -= nums[left]
