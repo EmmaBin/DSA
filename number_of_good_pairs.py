a3d2407d0e1482e9510f03f140aa6355085395ee
emmacodes <binma.emma@gmail.com>
2023-01-24 14:23:41 -0800

    hashmap

---
+++ number_of_good_pairs.py
@@ -0,0 +1,28 @@
+
+#https://leetcode.com/problems/number-of-good-pairs/description/
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        hashMap = {}
+        res = 0
+        for number in nums:            
+            if number in hashMap:
+                res += hashMap[number]
+                hashMap[number] += 1
+            else:
+                hashMap[number] = 1
+        return res
+
+
+#If the value already exists in the hashMap that means 
+# the number of new pairs is equal to the frequency since the current value 
+# can be paired with each prior occurrence .
+
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        hmap,count = {},0
+        for i,num in enumerate(nums):
+            if num in hmap:
+                count += len(hmap[num])
+                hmap[num].append(i)
+            else:
+                hmap[num] = [i]
+        return count
