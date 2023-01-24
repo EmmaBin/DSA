da864884acb7445d6932701ddbc35432a2c661cd
emmacodes <binma.emma@gmail.com>
2023-01-24 15:10:21 -0800

    lambda

--- sorted_chars_by_frequency.py
+++ sorted_chars_by_frequency.py
@@ -13,3 +13,12 @@ class Solution:
         for char in char_sorted:
             result +=(char*counter[char])
         return result
+
+
+    
+    def frequencySort(self, s: str) -> str:
+	sCounter = Counter(s)
+	result = []
+	for key, value in sorted(sCounter.items(), key=lambda x:x[1], reverse=True):
+		result.append(key*value)
+	return ''.join(result)
