65399d62038d8af4159f3e719b4b5c32b3d736cb
emmacodes <binma.emma@gmail.com>
2023-01-30 11:48:49 -0800

    need redo recursion

--- simple_adding.py
+++ simple_adding.py
@@ -16,3 +16,13 @@ def SimpleAdding(num):
 
 # keep this function call here 
 print(SimpleAdding(input()))
+
+def SimpleAdding(num): 
+    total = 0
+    for i in range(1, num+1):
+      total += i
+    # code goes here 
+    return total
+    
+# keep this function call here  
+print(SimpleAdding(input()))
