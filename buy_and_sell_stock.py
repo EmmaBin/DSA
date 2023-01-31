083f5777684f5155e625ad629475d8c882f8fa18
emmacodes <binma.emma@gmail.com>
2023-01-31 12:32:52 -0800

    second time

--- buy_and_sell_stock.py
+++ buy_and_sell_stock.py
@@ -16,3 +16,23 @@ def maxProfit(self, prices: List[int]) -> int:
 #1. float('inf') infinity used to initialize and update to find the minimum number
 #2. in this question, loop over the array once, while looping, keep holding of the min
 # price, and update the max_profit using max built-in method.
+
+
+# Two point
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #update left pointer to the newest low 
+        #then proceed the right pointer to one direction
+
+        left = 0
+        right =1
+        max_p = 0
+
+        while right < len(prices):
+            if prices[right] < prices[left]:
+                left = right
+                right +=1
+            else:
+                max_p = max(max_p, prices[right]- prices[left])
+                right+=1
+        return max_p
