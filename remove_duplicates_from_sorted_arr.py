a61a6508a0eb5a0ca399b3b7f7fe08e76b44317a
emmacodes <binma.emma@gmail.com>
2023-02-03 16:06:36 -0800

    two pointers

--- remove_duplicates_from_sorted_arr.py
+++ remove_duplicates_from_sorted_arr.py
@@ -27,3 +27,11 @@ def removeDuplicates(self, nums: List[int]) -> int:
             slow += 1
 
     return slow + 1
+
+def removeDuplicates(self, nums: List[int]) -> int:
+    j = 0
+    for i in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            j += 1
+            nums[j] = nums[i]
+    return j + 1
