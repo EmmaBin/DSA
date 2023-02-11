3298a4e46a0b726b1da96b8e3244f29dfbc5126b
emmacodes <binma.emma@gmail.com>
2023-02-11 13:40:30 -0800

    searching and regex

---
+++ alphabet_searching.py
@@ -0,0 +1,42 @@
+#Have the function AlphabetSearching(str) take the str parameter being passed and 
+# return the string true if every single letter of the English alphabet exists in the string, 
+# \otherwise return the string false. 
+# For example: if str is "zacxyjbbkfgtbhdaielqrm45pnsowtuv" then your program should return the string true 
+# because every character in the alphabet exists in this string even though some characters appear more than once.
+
+
+def AlphabetSearching(strParam):
+  new_str= strParam.lower()
+  result = ''
+  for letter in new_str:
+    if letter.isalpha():
+      result += letter
+  result = set(result)
+  if len(result)== 26:
+    return True
+  else:
+    return False
+
+  # code goes here
+ 
+
+# keep this function call here 
+print(AlphabetSearching(input()))
+
+
+import re
+
+def AlphabetSearching(str):
+  str = str.lower()
+  chars = set(re.findall('[a-z]', str))
+
+  if len(chars) != 26:
+    return 'false'
+  else:
+    return 'true'
+
+  # code goes here
+
+
+# keep this function call here 
+print(AlphabetSearching(input()))
