7dbc48c5b6cc370a890d1896ef709d522645e267
emmacodes <binma.emma@gmail.com>
2023-02-21 20:40:35 -0800

    string format() and math.floor

---
+++ division_stringified.py
@@ -0,0 +1,12 @@
+#Have the function DivisionStringified(num1,num2) take both parameters being passed, divide num1 by num2, and return the result as a string with properly formatted commas. 
+#If an answer is only 3 digits long, return the number with no commas (ie. 2 / 3 should output "1"). 
+#For example: if num1 is 123456789 and num2 is 10000 the output should be "12,346".
+
+def DivisionStringified(num1,num2):
+  import math
+
+  # code goes here
+  return "{:,}".format(math.floor((num1/num2) + .5))
+
+# keep this function call here 
+print(DivisionStringified(input()))
