972de2fd808ef556af70f29ceae6d4b03ca58c36
emmacodes <binma.emma@gmail.com>
2023-02-23 08:13:23 -0800

    regex, [^0-9] means anything that is not a number

---
+++ number_addition.py
@@ -0,0 +1,24 @@
+#Have the function NumberAddition(str) take the str parameter, 
+# search for all the numbers in the string, add them together, 
+# then return that final number. For example: if str is "88Hello 3World!" the output should be 91. 
+# You will have to differentiate between single digit numbers and multiple digit numbers like in the example above. 
+# So "55Hello" and "5Hello 5" should return two different answers. 
+# Each string will contain at least one letter or symbol.
+
+import re
+def NumberAddition(strParam):
+  number_list = re.split('[^0-9]', strParam)
+  total=0
+  for number in number_list:
+    if number:
+      total += int(number)
+
+  
+
+  # code goes here
+  return total
+
+# keep this function call here 
+print(NumberAddition(input()))
+#using regex to split the string into list, delimiter is non-numerical element
+#loop over the list to add up the number, str=>int
