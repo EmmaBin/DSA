8c04d1d063653d8c30ea61493e12d852f9268b37
emmacodes <binma.emma@gmail.com>
2023-02-25 11:00:14 -0800

    using set to find intersection

--- overlapping_ranges.py
+++ overlapping_ranges.py
@@ -30,3 +30,16 @@ print(OverlappingRanges(input()))
 #这道题我用了十五分钟，很开心，在遇到关卡的时候，我还能坚持想别的办法接着做，及时调整，及时改错，即使可能心里慌慌的我
 #还是接着做，我想这些也是面试官想看到的，想找的。
 #找到两个overlap的数字有几个 是这个问题的关键
+
+
+
+
+def OverlappingRanges(arr):
+  first_set = set(range(arr[0],arr[1]+1))
+  second_set = set(range(arr[2],arr[3]+1))
+  if len(first_set.intersection(second_set)) >= arr[4]:
+    return 'true'
+  return 'false'
+
+# keep this function call here 
+print(OverlappingRanges(input()))
