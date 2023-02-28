b0401bf6ce76b3de87cc95900472b8b4c1f4e58f
emmacodes <binma.emma@gmail.com>
2023-02-28 10:55:04 -0800

    review once

--- subarray_sum_equal_k.py
+++ subarray_sum_equal_k.py
@@ -14,3 +14,25 @@ class Solution:
             counts[curr] += 1
     
         return ans
+
+
+from collections import defaultdict
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        preSum = 0
+        counter = defaultdict(int)
+
+        for num in nums:
+            preSum += num
+            
+            # instead of initiate counter[0] with 1
+            if preSum == k:
+                res += 1
+
+            if preSum - k in counter:
+	            res += counter[preSum - k]
+
+            counter[preSum] += 1
+
+        return res
