bedde7eb9b5d33c353e2b30253b3a83d03efb40d
emmacodes <binma.emma@gmail.com>
2023-06-06 15:02:14 -0700

    review twice

--- two_sum.py
+++ two_sum.py
@@ -26,10 +26,22 @@ class Solution:
 
 class Solution(object):
     def twoSum(self, nums, target):
-        seen = {}
+        seen = {}class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            num1 = nums[i]
+            num2 = target - num1
+            
+            # Check if the complement exists in the remaining part of the list
+            if num2 in nums[i+1:]:
+                # Return the indices of the two numbers that add up to the target
+                return [i, nums.index(num2, i+1)]
         for i in range(len(nums)):
             diff = target - nums[i]
             if diff in seen:
                 return [seen[diff], i]
             else:
                 seen[nums[i]] = i
+#O(n**2)
+
+
