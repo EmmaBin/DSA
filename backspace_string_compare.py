df915945c11d75d20a9caca8b6fbc2c9612e806f
emmacodes <binma.emma@gmail.com>
2023-06-07 14:29:56 -0700

    review once and updated layer of checking

--- backspace_string_compare.py
+++ backspace_string_compare.py
@@ -34,3 +34,27 @@ class Solution:
             return "".join(stack)
 
         return build(s) == build(t)
+    
+
+#6/7 update: if the first char is #, it will throw error, so needed to check this situation
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s_backspaced = []
+        t_backspaced = []
+        
+        for i in range(len(s)):
+            if s[i] == '#':
+                if s_backspaced:
+                    s_backspaced.pop()
+            else:
+                s_backspaced.append(s[i])
+            
+        for i in range(len(t)):
+            if t[i] == '#':
+                if t_backspaced:
+                    t_backspaced.pop()
+            else:
+                t_backspaced.append(t[i])
+        
+        return s_backspaced == t_backspaced
