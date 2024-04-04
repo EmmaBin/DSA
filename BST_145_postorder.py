52c6aeb6c2b19bc7112b29bcf2eb4df1728bbb7b
emmacodes <binma.emma@gmail.com>
2024-04-04 12:04:06 -0700

    postorder

---
+++ BST_145_postorder.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result =[]
+
+        if root == None:
+            return []
+
+        result.extend(self.postorderTraversal(root.left))
+        result.extend(self.postorderTraversal(root.right))
+        result.append(root.val)
+        return result
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        left = self.postorderTraversal(root.left)
+        right = self.postorderTraversal(root.right)
+
+        return left + right + [root.val]
