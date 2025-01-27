826e410886fcf2848d3be92406bda110be5a0807
emmacodes <binma.emma@gmail.com>
2025-01-26 17:13:46 -0800

    slow and fast pointer

--- remove_duplicates_from_sorted_arr.py
+++ remove_duplicates_from_sorted_arr.py
@@ -1,19 +1,19 @@
-#https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 
-#这道题的关键是如果用下面的two pointers，要想什么时候advance pointers, 当右面的指针
-#所指的数字因为一样被pop之后，其实指针就已经自动指向新的一个数字了，所以无需再　advance任何指针，
-#接着进行compare就可以了
+# 这道题的关键是如果用下面的two pointers，要想什么时候advance pointers, 当右面的指针
+# 所指的数字因为一样被pop之后，其实指针就已经自动指向新的一个数字了，所以无需再　advance任何指针，
+# 接着进行compare就可以了
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        left=0
-        right=1
+        left = 0
+        right = 1
         while right < len(nums):
             if nums[left] == nums[right]:
                 nums.pop(right)
- 
+
             else:
-                left+=1
-                right+=1
+                left += 1
+                right += 1
 
 
 def removeDuplicates(self, nums: List[int]) -> int:
@@ -28,6 +28,7 @@ def removeDuplicates(self, nums: List[int]) -> int:
 
     return slow + 1
 
+
 def removeDuplicates(self, nums: List[int]) -> int:
     j = 0
     for i in range(1, len(nums)):
@@ -35,3 +36,13 @@ def removeDuplicates(self, nums: List[int]) -> int:
             j += 1
             nums[j] = nums[i]
     return j + 1
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 1
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[fast-1]:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
